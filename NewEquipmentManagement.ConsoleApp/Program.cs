66116d838b25124acd5cc11cb943c45b894f41ef
Otavio Brignoni <otavio.678.b@gmail.com>
2025-04-24 09:33:04 -0300

    Adde main menu

--- NewEquipmentManagement.ConsoleApp/Program.cs
+++ NewEquipmentManagement.ConsoleApp/Program.cs
@@ -1,9 +1,11 @@
-﻿namespace NewEquipmentManagement.ConsoleApp;
+﻿using NewEquipmentManagement.ConsoleApp.Shared;
+
+namespace NewEquipmentManagement.ConsoleApp;
 
 class Program
 {
     static void Main(string[] args)
     {
-
+        MainMenu.Show();
     }
 }
---
+++ NewEquipmentManagement.ConsoleApp/Shared/MainMenu.cs
@@ -0,0 +1,43 @@
+﻿using NewEquipmentManagement.ConsoleApp.ManufaturerModule;
+
+namespace NewEquipmentManagement.ConsoleApp.Shared;
+public class MainMenu
+{
+
+    public static ManufacturerInterface ManufacturerInterface = new ManufacturerInterface();
+
+
+    public static void Show()
+    {
+        string title = "Management";
+        bool leaveOptionSelected = false;
+        while (!leaveOptionSelected)
+        {
+            Console.Clear();
+            Utils.Header(title);
+            Console.WriteLine();
+            Console.WriteLine("1 - Manage Manufacturers");
+            Console.WriteLine("2 - Manage Equipments");
+            Console.WriteLine("3 - Manage Calls");
+            Console.WriteLine("4 - Leave");
+            switch (Utils.GetValidOption(1, 4))
+            {
+                case 1:
+                    ManufacturerInterface.Show();
+                    break;
+                case 2:
+                    //EquipmentInterface.Show();
+                    break;
+                case 3:
+                    //CallInterface.Show();
+                    break;
+                case 4:
+                    Console.Clear();
+                    Console.WriteLine("See you next time!");
+                    Thread.Sleep(634);
+                    leaveOptionSelected = true;
+                    break;
+            }
+        }
+    }
+}
